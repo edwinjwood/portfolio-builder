d65d25b9858f44427a8e582192c8cbc72d733dad
Edwin Wood <edwinjwood@gmail.com>
2025-08-12 13:53:36 -0400

    updated to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Projects from './Projects';
 
 
