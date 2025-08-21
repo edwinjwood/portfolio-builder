877096cce83cb56fbafb436f9a7864d466c39337
Edwin Wood <edwinjwood@gmail.com>
2025-08-21 13:40:43 -0400

    deploy test 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import viteLogo from '/vite.svg';
 
 const Footer = () => (
   <footer className="text-center text-xs text-gray-500 dark:text-gray-400 pb-6 mt-4 sm:mt-8 opacity-90">
-  <div>© {new Date().getFullYear()} LAYoR</div>
+  <div>© {new Date().getFullYear()} LAYoooR</div>
   </footer>
 );
 
